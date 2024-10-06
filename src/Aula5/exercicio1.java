d914aa773f85fb0e79a4e500de73d22b898cd043
rafaeltech41 <rafaelmoreiralopes41@gmail.com>
2024-10-06 09:19:29 -0300

    contagem regressiva

---
+++ src/Aula5/exercicio1.java
@@ -0,0 +1,12 @@
+package Aula5;
+
+public class exercicio1 {
+    //contagem regressiva
+
+    public static void main(String[]args){
+        for (int i = 10; i > 0; i--) {
+            System.out.println(i);
+
+        }
+    }
+}
