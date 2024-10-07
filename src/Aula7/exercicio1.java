0da067fd58a26ff4eea23c53a7c37c9aa805863e
rafaeltech41 <rafaelmoreiralopes41@gmail.com>
2024-10-07 15:40:25 -0300

    Função que imprime uma mensagem

---
+++ src/Aula7/exercicio1.java
@@ -0,0 +1,11 @@
+package Aula7;
+
+public class exercicio1 {
+
+    public static void main(String[] args) {
+        funcao();
+    }
+    public static void funcao(){
+        System.out.println("Bem vindo ao sistema!");
+    }
+}
