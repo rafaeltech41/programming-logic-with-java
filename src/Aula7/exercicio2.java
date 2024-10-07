39357a9b5cf34bc9fe9aa8ef003aa1141bc9f895
rafaeltech41 <rafaelmoreiralopes41@gmail.com>
2024-10-07 15:41:19 -0300

    Função que soma dois números

---
+++ src/Aula7/exercicio2.java
@@ -0,0 +1,11 @@
+package Aula7;
+
+public class exercicio2 {
+    public static void main(String[] args) {
+       int x = soma(2,2);
+        System.out.println(x);
+    }
+    public static int soma(int n1,int n2){
+        return n1+n2;
+    }
+}
