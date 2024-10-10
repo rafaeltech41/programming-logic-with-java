2745803914f5e9dc20fd9c13238633f7a2a161b6
Rafaeltech41 <rafaelmoreiralopes41@gmail.com>
2024-10-10 15:20:10 -0300

    casting explícito

--- src/Aula2/exercicio4.java
+++ src/Aula2/exercicio4.java
@@ -4,6 +4,7 @@ public class exercicio4 {
     public static void main(String[] args) {
         double x = 1.5;
         int y = (int)x;
+        System.out.println(x);
         System.out.println(y);
     }
 }
