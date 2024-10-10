4ef61e2de2f8a688f2b7c6d99472844cfe26c428
Rafaeltech41 <rafaelmoreiralopes41@gmail.com>
2024-10-10 16:21:44 -0300

    converção de tipos (casting explicito casting implicito)

---
+++ src/Aula2/exercicio9.java
@@ -0,0 +1,12 @@
+package Aula2;
+
+public class exercicio9 {
+    public static void main(String[] args) {
+        float variavelX = 1.8F;
+        long variavelY = (long) variavelX;
+        double variavelZ = (double) variavelY;
+        System.out.println(variavelX);
+        System.out.println(variavelY);
+        System.out.println(variavelZ);
+    }
+}
