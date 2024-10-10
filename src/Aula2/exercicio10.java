43f78274139202c956342d837b9ef4f1d33b8ddb
Rafaeltech41 <rafaelmoreiralopes41@gmail.com>
2024-10-10 16:31:31 -0300

    diferença entre tipos de dados

---
+++ src/Aula2/exercicio10.java
@@ -0,0 +1,9 @@
+package Aula2;
+
+public class exercicio10 {
+    public static void main(String[] args) {
+        short varX = 155;
+        byte varY = (byte) varX;
+        System.out.println(varY);
+    }
+}
