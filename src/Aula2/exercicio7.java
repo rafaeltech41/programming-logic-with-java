1aed5d7293ae0e600cef4c49d07613b3d6d52b45
Rafaeltech41 <rafaelmoreiralopes41@gmail.com>
2024-10-10 16:08:02 -0300

    operadores lógicos básicos

---
+++ src/Aula2/exercicio7.java
@@ -0,0 +1,13 @@
+package Aula2;
+
+public class exercicio7 {
+    public static void main(String[] args) {
+        boolean cond1 = true;
+        boolean cond2 = true;
+        boolean cond3 = false;
+        System.out.println(cond1&&cond2);
+        System.out.println(cond3||cond2);
+
+
+    }
+}
