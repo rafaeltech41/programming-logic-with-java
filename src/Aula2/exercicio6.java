4a88d8a811b4bad023d49f46519dadbbb4bcf6c5
Rafaeltech41 <rafaelmoreiralopes41@gmail.com>
2024-10-10 15:39:25 -0300

    comparação de dois números

---
+++ src/Aula2/exercicio6.java
@@ -0,0 +1,22 @@
+package Aula2;
+
+public class exercicio6 {
+    public static void main(String[] args) {
+int x  = 10;
+int y = 11;
+        System.out.print("x>y ");
+        System.out.println(x>y);
+        System.out.print("x<y ");
+        System.out.println(x<y);
+        System.out.print("x==y ");
+        System.out.println(x==y);
+        System.out.print("x!=y ");
+        System.out.println(x!=y);
+
+
+
+
+
+
+    }
+}
