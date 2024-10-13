385c13d345823bd7a09052ec5920b99d22860a7d
rafaeltech41 <rafaelmoreiralopes41@gmail.com>
2024-10-12 21:49:23 -0300

    determinação de turno

---
+++ src/Aula4/exercicio9.java
@@ -0,0 +1,65 @@
+package Aula4;
+
+import java.util.Scanner;
+
+public class exercicio9 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("insira a hora atual do dia:\n(em formato 24 horas)");
+        Byte turno = scanner.nextByte();
+        if (turno==5){
+            System.out.println("você vai trabalhar no turno da manhã(5h-12h)");
+        }else if (turno==6){
+            System.out.println("você vai trabalhar no turno da manhã(5h-12h)");
+        }else if (turno==7){
+            System.out.println("você vai trabalhar no turno da manhã(5h-12h)");
+        }else if (turno==8){
+            System.out.println("você vai trabalhar no turno da manhã(5h-12h)");
+        }else if (turno==9){
+            System.out.println("você vai trabalhar no turno da manhã(5h-12h)");
+        }else if (turno==10){
+            System.out.println("você vai trabalhar no turno da manhã(5h-12h)");
+        }else if (turno==11){
+            System.out.println("você vai trabalhar no turno da manhã(5h-12h)");
+        }else if (turno==12){
+            System.out.println("você vai trabalhar no turno da tarde(12h-18h)");
+        }else if (turno==13){
+            System.out.println("você vai trabalhar no turno da tarde(12h-18h)");
+        }else if (turno==14){
+            System.out.println("você vai trabalhar no turno da tarde(12h-18h)");
+        }else if (turno==15){
+            System.out.println("você vai trabalhar no turno da tarde(12h-18h)");
+        }else if (turno==16){
+            System.out.println("você vai trabalhar no turno da tarde(12h-18h)");
+        }else if (turno==17){
+            System.out.println("você vai trabalhar no turno da tarde(12h-18h)");
+        }else if (turno==18){
+            System.out.println("você vai trabalhar no turno da noite(18h-22h)");
+        }else if (turno==19){
+            System.out.println("você vai trabalhar no turno da noite(18h-22h)");
+        }else if (turno==20){
+            System.out.println("você vai trabalhar no turno da noite(18h-22h)");
+        }else if (turno==21){
+            System.out.println("você vai trabalhar no turno da noite(18h-22h)");
+        }else if (turno==22){
+            System.out.println("você vai trabalhar no turno da madrugada(22h-5h)");
+        }else if (turno==23){
+            System.out.println("você vai trabalhar no turno da madrugada(22h-5h)");
+        }
+        else if (turno==0){
+            System.out.println("você vai trabalhar no turno da madrugada(22h-5h)");
+        }
+        else if (turno==1){
+            System.out.println("você vai trabalhar no turno da madrugada(22h-5h)");
+        }else if (turno==2){
+            System.out.println("você vai trabalhar no turno da madrugada(22h-5h)");
+        }else if (turno==3){
+            System.out.println("você vai trabalhar no turno da madrugada(22h-5h)");
+        }else if (turno==4){
+            System.out.println("você vai trabalhar no turno da madrugada(22h-5h)");}
+        else {
+            System.out.println("fim do sistema.");
+        }
+
+    }
+}
