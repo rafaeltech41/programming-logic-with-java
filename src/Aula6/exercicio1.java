104b17fb2a2e1bb4b9f65f1253e2f4176b7ededa
rafaeltech41 <rafaelmoreiralopes41@gmail.com>
2024-10-15 09:47:17 -0300

    Soma de elementos inteiros em um vetor

---
+++ src/Aula6/exercicio1.java
@@ -0,0 +1,13 @@
+package Aula6;
+
+public class exercicio1 {
+    public static void main(String[] args) {
+        int [] numeros = new int[5];
+        numeros[0] = 10;
+        numeros[1] = 10;
+        numeros[2] = 10;
+        numeros[3] = 10;
+        numeros[4] = 10;
+        System.out.println(numeros[0]+numeros[1]+numeros[2]+numeros[3]+numeros[4]);
+    }
+}
