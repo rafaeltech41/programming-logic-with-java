de5fcafa195909bfd205d9eef35522396e1ab5c0
rafaeltech41 <rafaelmoreiralopes41@gmail.com>
2024-10-21 15:24:05 -0300

    criar e escrever em um arquivo  de texto

--- src/Aula8/exercicio1.java
+++ src/Aula8/exercicio1.java
@@ -1,21 +1,38 @@
 package Aula8;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
+import java.util.Scanner;
 
 public class exercicio1 {
     public static void main(String[] args) {
-    File arquivo = new File("aluno.txt");
-    if (arquivo.exists()){
-        System.out.println("fim do sistema");
-    } else  {
+        Scanner scanner = new Scanner(System.in);
+
+
         try {
-            if (arquivo.createNewFile()){
-                System.out.println(arquivo.getName());
+            File alunos = new File("C:\\Users\\Aluno\\Java\\src\\Aula8\\alunos.txt");
+            FileWriter writer = new FileWriter(alunos, true);
+            for (int i = 1; i < 6; i++) {
+                System.out.println("insira o nome do aluno " + i );
+                  writer.write(scanner.next()+"\n");
+
+            }
+            writer.close();
+
+            if (alunos.exists()) {
+
+            } else {
+                if (alunos.createNewFile()) {
+
+                } else {
+
+                }
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-    }
+
     }
 }
