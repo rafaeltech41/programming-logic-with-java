c659aa8968cd5a409c3db38a11c9d7827e2f9f66
rafaeltech41 <rafaelmoreiralopes41@gmail.com>
2024-10-21 17:16:11 -0300

    desafio2

---
+++ src/Aula7/desafio2.java
@@ -0,0 +1,36 @@
+package Aula7;
+
+import java.util.Scanner;
+
+public class desafio2 {
+    public static void main(String[] args) {
+        cadastrarUsuario();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("digite a quantidade de pessoas a serem cadastradas:");
+        int qtdPessoas = scanner.nextInt();
+        scanner.nextLine();
+        String[] cabecalho = {"ID", "Nome", "Telefone", "Email"};
+        String[][] matrizCadastro = new String[qtdPessoas + 1][cabecalho.length];
+        System.out.println("preencha os dados a seguir: ");
+        matrizCadastro[0] = cabecalho;
+        for (int linhas = 1; linhas < matrizCadastro.length; linhas++) {
+            for (int colunas = 0; colunas < cabecalho.length; colunas++) {
+                System.out.print(cabecalho[colunas] + ": ");
+                matrizCadastro[linhas][colunas] = scanner.nextLine();
+            }
+        }
+        String tabela = "";
+        for (int linhas = 0; linhas < matrizCadastro.length; linhas++) {
+            for (int colunas = 0; colunas < cabecalho.length; colunas++) {
+                tabela += matrizCadastro[linhas][colunas];
+                tabela += colunas == 0 ? "\t" : "\t\t\t";
+
+            }
+            tabela+="\n";
+        }
+        System.out.println(tabela);
+    }
+
+    private static void cadastrarUsuario() {
+    }
+}
