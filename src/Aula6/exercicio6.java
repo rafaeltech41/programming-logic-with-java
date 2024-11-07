b5ede7b37ebf5f06f8321c549bbe67b0b7084eaf
rafaeltech41 <rafaelmoreiralopes41@gmail.com>
2024-11-07 10:48:44 -0300

    preenchendo uma matriz

--- src/Aula6/exercicio6.java
+++ src/Aula6/exercicio6.java
@@ -6,15 +6,18 @@ public class exercicio6 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int[][] matriz = new int[2][2];
+        matriz[0][0] = scanner.nextInt();
+        matriz[0][1] = scanner.nextInt();
+        matriz[1][0] = scanner.nextInt();
+        matriz[1][1] = scanner.nextInt();
         for (int l = 0; l < matriz.length; l++) {
             for (int c = 0; c < matriz.length; c++) {
-                int linha =0;
-                System.out.println("insira numero "+""+c);
-
-                matriz[0][c] = scanner.nextInt();
+                System.out.println(matriz[l][c]);
             }
         }
-        System.out.println(matriz[0][0]);
+
+
+
 
     }
 }
